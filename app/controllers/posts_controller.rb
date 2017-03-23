6bb986fff8c3ca7d4f14744b6676fc778045072a
Dmitry Kudriavtsev <dkudriavtsev@gmail.com>
2017-03-23 09:51:09 -0700

    Added password

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   layout 'index', :only => [:listview]
+  http_basic_authenticate_with name: "gibsonek", password: "securepassword", only: :new
 
   def listview
   end
